45841a3c0e8a894b0c7b9324732e31cd07b74f1b
Lenovo <Lenovo@DESKTOP-NLG0QKH>
2023-03-04 00:29:29 +0530

    Complete Singleton implementaion

---
+++ src/com/designpatterns/creational/singleton/MySingleton.java
@@ -0,0 +1,18 @@
+package com.designpatterns.creational.singleton;
+
+public class MySingleton {
+	
+	private static MySingleton instance;
+	
+	private MySingleton() {
+		
+	}
+
+	public static MySingleton getInstance() {
+		
+		if(instance == null) {
+			instance = new MySingleton();
+		}
+		return instance;
+	}
+}
--- src/com/designpatterns/creational/singleton/SingletonDemo.java
+++ src/com/designpatterns/creational/singleton/SingletonDemo.java
@@ -3,8 +3,17 @@ package com.designpatterns.creational.singleton;
 public class SingletonDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		System.out.println("Demo for Singleton");
+		MySingleton o1 = MySingleton.getInstance();
+		MySingleton o2 = MySingleton.getInstance();
+		MySingleton o3 = MySingleton.getInstance();
+		MySingleton o4 = MySingleton.getInstance();
+		
+		System.out.println(o1.hashCode());
+		System.out.println(o2.hashCode());
+		System.out.println(o3.hashCode());
+		System.out.println(o4.hashCode());
 	}
 
 }
